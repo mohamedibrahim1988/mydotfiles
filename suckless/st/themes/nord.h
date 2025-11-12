06ead7fc1c58f8a0822650ec98fc8df0c537b2a0
mohamedibrahim1988 <hmam88@gmail.com>
2025-11-13 01:12:26 +0200

    nord themes

---
+++ suckless/st/themes/nord.h
@@ -0,0 +1,30 @@
+static const char *colorname[] = {
+   	"#3b4252", /* black   */
+	"#bf616a", /* red     */
+	"#a3be8c", /* green   */
+	"#ebcb8b", /* yellow  */
+	"#81a1c1", /* blue    */
+	"#b48ead", /* magenta */
+	"#88c0d0", /* cyan    */
+	"#e5e9f0", /* white   */
+ 
+ 	/* 8 bright colors */
+	"#4c566a", /* black   */
+	"#bf616a", /* red     */
+	"#a3be8c", /* green   */
+	"#ebcb8b", /* yellow  */
+	"#81a1c1", /* blue    */
+	"#b48ead", /* magenta */
+	"#8fbcbb", /* cyan    */
+	"#eceff4", /* white   */
+
+    [255] = 0,
+
+	"#d8dee9", /* 256 -> cursor */
+	"#B4BEFE", /* 257 -> rev cursor */
+	"#d8dee9",  /* 258 -> foreground */
+	"#2e3440",   /* 259 -> background */
+	"#2e3440",   /* 260 -> background unfocused */
+	"#eceff4",  /* 261 -> visual bell */
+
+};
