dcc7f5837d289283f6a198cf74eef119cdf7f403
mohamedibrahim1988 <hmam88@gmail.com>
2025-11-13 01:26:12 +0200

    dracula theme

---
+++ suckless/st/themes/dracula.h
@@ -0,0 +1,29 @@
+static const char *colorname[] = {
+    /* 8 normal colors */
+    "#000000", /* black   */
+    "#ff5555", /* red     */
+    "#50fa7b", /* green   */
+    "#f1fa8c", /* yellow  */
+    "#bd93f9", /* blue    */
+    "#ff79c6", /* magenta */
+    "#8be9fd", /* cyan    */
+    "#bbbbbb", /* white   */
+                                  
+    /* 8 bright colors */
+    "#44475a", /* black   */
+    "#ff5555", /* red     */
+    "#50fa7b", /* green   */
+    "#f1fa8c", /* yellow  */
+    "#bd93f9", /* blue    */
+    "#ff79c6", /* magenta */
+    "#8be9fd", /* cyan    */
+    "#ffffff", /* white   */
+        	[255] = 0,                    
+	"#f8f8f2", /* 256 -> cursor */
+	"#B4BEFE", /* 257 -> rev cursor */
+	"#f8f8f2", /* 258 -> foreground */
+	"#282a36", /* 259 -> background */
+	"#282a36", /* 260 -> background unfocused */
+	"#f8f8f2", /* 261 -> visual bell */
+
+};
